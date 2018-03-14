ea86f91e7f2e69d13ecf571a1f669324bfc520b1
fyAgent <1791836545@qq.com>
2018-03-14 18:12:29 +0800

    同步数据库

---
+++ static/socket.sql
@@ -0,0 +1,42 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2018-03-14 18:11:14
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "chat"
+#
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE `chat` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickName` varchar(255) DEFAULT NULL,
+  `msg` varchar(255) DEFAULT NULL,
+  `vip` int(11) DEFAULT '0',
+  `time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "session"
+#
+
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickName` varchar(255) DEFAULT NULL,
+  `headImg` varchar(255) DEFAULT '' COMMENT '澶村儚鍦板潃',
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
